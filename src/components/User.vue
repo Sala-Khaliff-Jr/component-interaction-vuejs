1998ac31b3f14659c6ed0138ff11366cdfe5c9c4
Sala-Khaliff-Jr <salakhaliff@gmail.com>
2020-04-16 02:07:52 +0530

    component data passing has been added

---
+++ src/components/User.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <hr>
+        <button @click="changeName">
+            change user
+        </button>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <!-- v-bind:name="name" parameter passed to the child -->
+                <!-- @resetname is in child that resets name  -->
+                <app-user-detail :name="name" @resetname="name = $event"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data:function(){
+            return{
+                name:'sala'
+            };
+        },
+        methods:{
+            changeName(){
+                this.name = 'sowmy'
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <h2>user Name {{swapname()}}</h2>
+        <button @click="resetName">Reset name</button>
+    </div>
+</template>
+
+<script>
+export default{
+    // can also be an object
+    // props : ['name']
+    props :{
+        // propname : [data types]
+        name : [String,Array]
+    },
+    methods:{
+        swapname(){
+            return this.name;
+        },
+        resetName(){
+            this.name = 'sala';
+            // emit allows to send data back to parent
+            // first param is the event name,second param is the data passed through event (accessed through $ sign)
+            this.$emit('resetname',this.name);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
